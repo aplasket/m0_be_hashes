c4eabe9c9959cbbd9b363a3ba19c9f8ec7b7dd72
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-01 13:47:39 -0500

    Use each method to print keys and values in examples

--- hashes_2.rb
+++ hashes_2.rb
@@ -8,9 +8,14 @@ states = {
 }
 
 # Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
-#
-#
-#
+states.each do |state, abbreviation|
+  p "#{state}'s abbreviation is #{abbreviation}"
+end
+
+# "Oregon's abbreviation is OR"
+# "Alabama's abbreviation is AL"
+# "New Jersey's abbreviation is NJ"
+# "Colorado's abbreviation is CO"
 
 
 # ======================================
@@ -24,9 +29,15 @@ birthdays = {
 }
 
 # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
-#
-#
-#
+
+birthdays.each do |name, birthdate|
+  p "#{name}'s birthday is on #{birthdate}. Let's celebrate!"
+end
+
+# "Cindy's birthday is on June 10. Let's celebrate!"
+# "Rigo's birthday is on March 31. Let's celebrate!"
+# "Amos's birthday is on July 21. Let's celebrate!"
+# "Jeff's birthday is on February 12. Let's celebrate!"
 
 
 # ======================================
@@ -40,6 +51,14 @@ login_statuses = {
 }
 
 # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
-#
-#
-#
+
+login_statuses.each do |name, status|
+  if status
+    p "hey #{name}, thanks for logging in!"
+  end
+end
+
+# "hey Cindy, thanks for logging in!"
+# "hey Amos, thanks for logging in!"
+# "hey Jeff, thanks for logging in!"
+# it did not print Rigo because rigo is not logged in (boolean is false)
